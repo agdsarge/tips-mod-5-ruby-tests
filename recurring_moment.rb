9da2db3909b1a713558077f14c1aa1420af743f2
agdsarge <agdsargeant@gmail.com>
2020-07-20 03:30:05 -0400

    passes all rspec tests

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -2,29 +2,42 @@ require 'active_support'
 require 'active_support/core_ext'
 
 class RecurringMoment
-  def initialize(start:, interval:, period:)
-    @start = start
-    @interval = interval
-    @period = period
-  end
-
-  def match(date)
-    current = @start
-
-    while current < date
-      if current == date
-        return true
-      end
-
-      if @period == 'monthly'
-        current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
-        current = current.advance(months: @interval)
-      elsif @period == 'daily'
-        current = current.advance(days: @interval)
-      end
+    def initialize(start:, interval:, period:) # accepts a hash
+        @start = start          # DateTime object
+        @interval = interval    # integer
+        @period = period        # string. one of  ['daily', 'weekly', 'monthly']
     end
 
-    return false
-  end
+    def month_helper(date, index)
+        date.advance(months: @interval * index)
+    end
+
+
+    def match(date)
+        # instance method. returns true or false.
+        # the param date is a DateTime object
+            # in the test, date is a DateTime of the form DateTime.advance(period: interval)
+        # the advance method for DateTime is imported from active_support or active_support/core_ext
+        # something is weird about months. i suspect it's february. i've partitioned it off.
+
+        current = @start
+        ind = 0
+
+        unless @period == 'monthly'
+            while current < date
+                if @period == 'weekly'
+                    current = current.advance(weeks: @interval)
+                elsif @period == 'daily'
+                    current = current.advance(days: @interval)
+                end
+            end
+        else
+            while current < date
+                current = month_helper(@start, ind)
+                ind += 1
+            end
+        end
+
+        current == date
+    end
 end
